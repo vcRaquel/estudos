340f72dd8f20bfaaaa8ad705d69931d34776f271
vcRaquel <vc.raquel@gmail.com>
2021-06-29 16:18:22 -0300

    conclusão nlw6

--- rocketseat/NLW/maratona-discover-nlw6/src/server.js
+++ rocketseat/NLW/maratona-discover-nlw6/src/server.js
@@ -8,9 +8,10 @@ server.set('view engine', 'ejs')
 
 server.use(express.static("public"))
 
-// path é o caminho do arquivo no computador( pastas, etc), o join irá juntar o path com o dirname que é o caminho da página do projeto dentro do projeto
 server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, ()=> console.log("RODANDO")) 
+server.listen(3000, () => console.log("RODANDO"))
